7418124bf19f98b0e84782e29b0d0dba8f99f798
erikproper <erikproper@gmail.com>
2025-11-27 20:35:34 +0100

    Commit Comments

--- big_modelling_bus_1.0/mbconnect/generic_constants.go
+++ big_modelling_bus_1.0/mbconnect/generic_constants.go
@@ -15,7 +15,7 @@
 package mbconnect
 
 const (
-	ModellingBusVersion = "bus-version-1.0"
-	filePayload         = "payload"
-	jsonFileName        = "message.json"
+	ModellingBusVersion = "bus-version-1.0" // The current version of the BIG modelling bus.
+	filePayload         = "payload"         // Name of the file used to store the "payload" of artefacts on the FTP server.
+	jsonFileName        = "message.json"    // Name of the local file used to (temporarily) represent upload/downloaded JSONs.
 )
