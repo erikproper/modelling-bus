b37d3a33bf9dc7bb93a0d81848a339a7d1257c7d
erikproper <erikproper@gmail.com>
2025-12-05 09:59:31 +0100

    $1

--- connect/layer_3_observations.go
+++ connect/layer_3_observations.go
@@ -55,18 +55,44 @@ func (b *TModellingBusConnector) PostJSONObservation(observationID string, json 
 	b.postJSON(b.jsonObservationsTopicPath(observationID), json, generics.GetTimestamp())
 }
 
-func (b *TModellingBusConnector) ListenForRawObsverationPostings(agentID, topicPath string, postingHandler func(string)) {
-	b.listenForFilePostings(agentID, topicPath, generics.JSONFileName, func(localFilePath, _ string) {
+/*
+ * Listening to observations related postings
+ */
+
+func (b *TModellingBusConnector) ListenForRawObservationPostings(agentID, observationID string, postingHandler func(string)) {
+	b.listenForFilePostings(agentID, b.jsonObservationsTopicPath(observationID), generics.JSONFileName, func(localFilePath, _ string) {
 		postingHandler(localFilePath)
 	})
 }
 
+// HERE!
+func (b *TModellingBusConnector) ListenForJSONObservationPostings(agentID, topicPath string, postingHandler func([]byte, string)) {
+	b.listenForFilePostings(agentID, topicPath, generics.JSONFileName, func(localFilePath, timestamp string) {
+		postingHandler(getJSONFromTemporaryFile(localFilePath, timestamp))
+	})
+
+	//	b.listenForJSONPostings(agentID, b.jsonArtefactsUpdateTopicPath(artefactID), func(json []byte, _ string) {
+	//		if b.updateUpdatedJSONArtefact(json) {
+	//			handler()
+	//		}
+	//	})
+
+	//	func (b *TModellingBusConnector) listenForJSONPostings(agentID, topicPath string, postingHandler func([]byte, string)) {
+	//	b.modellingBusEventsConnector.listenForEvents(agentID, topicPath, func(message []byte) {
+	//		postingHandler(b.getJSONFromTemporaryFile(b.getLinkedFileFromRepository(message, generics.JSONFileName)))
+	//	})
+
+	// func (b *TModellingBusConnector) getJSONFromTemporaryFile(tempFilePath, timestamp string) ([]byte, string) {
+	//}
+
+}
+
 //
-// func (b *TModellingBusConnector) GetRawObsveration(agentID, topicPath, localFileName string) string {
+// func (b *TModellingBusConnector) GetRawObservation(agentID, topicPath, localFileName string) string {
 // 	localFilePath, _ := b.getFileFromPosting(agentID, topicPath, localFileName)
 // 	return localFilePath
 // }
 //
-// func (b *TModellingBusConnector) DeleteRawObsveration(topicPath string) {
+// func (b *TModellingBusConnector) DeleteRawObservation(topicPath string) {
 // 	b.deletePosting(topicPath)
 // }
